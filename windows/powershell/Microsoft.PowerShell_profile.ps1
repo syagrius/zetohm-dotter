5eecc0ed8cfca43004e53d955ca5a25b4e567bd4
Syagrius <syagrius@pm.me>
2025-10-15 11:52:07 +0200

    Correctif suppression de OneDrive

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,17 @@
 # Load FNM if package is active
-$fnmInit = "$env:USERPROFILE\.config\powershell\fnm-init.ps1"
+$fnmInit = Join-Path $env:USERPROFILE ".config\powershell\fnm-init.ps1"
 if (Test-Path $fnmInit) {
     . $fnmInit
 }
 
 # Load Zoxide if package is active
-$zoxideInit = "$env:USERPROFILE\.config\powershell\zoxide-init.ps1"
+$zoxideInit = Join-Path $env:USERPROFILE ".config\powershell\zoxide-init.ps1"
 if (Test-Path $zoxideInit) {
     . $zoxideInit
 }
 
 # Load Starship if package is active
-$starshipInit = "$env:USERPROFILE\.config\powershell\starship-init.ps1"
+$starshipInit = Join-Path $env:USERPROFILE ".config\powershell\starship-init.ps1"
 if (Test-Path $starshipInit) {
     . $starshipInit
 }
